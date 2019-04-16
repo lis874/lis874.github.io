e4fcfc8ea3dfde401971791b07863096fd9d9c9a
lis874 <lis874@newschool.edu>
2019-04-16 08:48:12 -0400

    update weather

--- projects/weather/main.js
+++ projects/weather/main.js
@@ -2,14 +2,14 @@ $(function(){
 	console.log('hello')
 });
 
-var url= 'https://api.openweathermap.org/data/2.5/weather?zip=11101&units=imperial&APPID=304f0ab7f8f51d03ea3494dcc3847c71'
+var url= 'https://api.openweathermap.org/data/2.5/weather?zip=10011&units=imperial&APPID=304f0ab7f8f51d03ea3494dcc3847c71';
 
 $.get(url, function(data) {
   // The get request fires a callback function when the API request finishes
   console.log(data);
   // The data object contains all the information returned in the API
   var weather = data.weather[0];
-  var weathertext = data.main;
+  var weathertext = data.weather[0].main;
   var humidity = data.main.humidity;
   var temp = data.main.temp;
 
@@ -19,33 +19,21 @@ $.get(url, function(data) {
   $('body').append(temp + 'F');
   $('body').append('<a href="http://teaching.synopticoffice.com/course/f18_pucd_2035c/projects/li/final/index.html" class="link>" What? </a>');
 
- 
-});
-
-
-$.get(url, function(results){
-
 	
-	weathertext = results.weather[0].main;
-
 
 for(var counter=0; counter< 1; counter++){
 	console.log('counter', counter);
 	console.log('weathertext', weathertext);
 
+        if(weathertext == 'Rain'){
 
-          if(weathertext == 'Rain'){
-
-             $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/rainingmen.mp4" type="video/mp4"></video></div>');
+          $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/rainingmen.mp4" type="video/mp4"></video></div>');
                     
-        }
-    
-        	if(weathertext == 'Clear'){
-                $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/walkingonsunshine.mp4" type="video/mp4"></video></div>');
-             }
-
-          if(weathertext == 'Clouds'){
-                $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/cloudnine.mp4" type="video/mp4"></video></div>');
+        } else if(weathertext == 'Clear'){
+            $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/walkingonsunshine.mp4" type="video/mp4"></video></div>');
+        
+        } else if(weathertext == 'Clouds'){
+            $('body').append('<div class="videos"><video width="520" height="440" controls autoplay loop class="video1"><source src="assets/cloudnine.mp4" type="video/mp4"></video></div>');
              }
       }
 });
